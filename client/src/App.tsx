8f5d683e76c3809fc9311052e2659ebe7f165b68
Pranitha Maganty <pranithamaganty@gmail.com>
2021-12-23 23:38:50 -0600

    create react router for page routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Search from "./pages/Search";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={Home}/>
+          <Route path="/create" element={Create} />
+          <Route path="/search" element={Search} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
