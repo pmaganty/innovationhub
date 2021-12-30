adaf731fc32d94f6e28d3589dd4989cda92bec51
Pranitha Maganty <pranithamaganty@gmail.com>
2021-12-30 14:36:30 -0600

    change postgres connection from local to heroku

--- models/db.ts
+++ models/db.ts
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "maganty4",
-    host: "localhost",
-    port: 5432,
-    database: "ihub"
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnathorized: false
+    }
 });
+
+/*
+user: "postgres",
+password: "maganty4",
+host: "localhost",
+port: 5432,
+database: "ihub"
+*/
 
 module.exports = pool;
