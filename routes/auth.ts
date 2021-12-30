31f72eb58269691e126470da0e560fce48300f1e
Pranitha Maganty <pranithamaganty@gmail.com>
2021-12-30 14:11:35 -0600

    pass different google oauth client codes for heroku

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,13 +2,13 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require("passport");
 const ihubController = require("./ihubController");
 
-const GOOGLE_CLIENT_ID = '183628566117-h39cergc60dpenjq1n0sjokgro69961a.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+const GOOGLE_CLIENT_ID = '751940641317-ppl10nn6su6jvdgl848tirqv6d0oa8oo.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://innovationhub1123.herokuapp.com/auth/google/callback"
+    callbackURL: "/auth/google/callback"
   },
   function(accessToken: any, refreshToken: any, profile: any, done: any) {
     console.log(profile);
