eb7add8fcf92a39b69fe65fe357e84aef574d906
Pranitha Maganty <pranithamaganty@gmail.com>
2021-12-30 14:40:10 -0600

    make sure tsc compiles new postgres pool definition

--- build/models/db.js
+++ build/models/db.js
@@ -1,10 +1,16 @@
 "use strict";
 var Pool = require("pg").Pool;
 var pool = new Pool({
-    user: "postgres",
-    password: "maganty4",
-    host: "localhost",
-    port: 5432,
-    database: "ihub"
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnathorized: false
+    }
 });
+/*
+user: "postgres",
+password: "maganty4",
+host: "localhost",
+port: 5432,
+database: "ihub"
+*/
 module.exports = pool;
