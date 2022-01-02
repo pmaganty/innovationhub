a6c591a541e09facbb7936d23bbaf839060bc52e
Pranitha Maganty <pranithamaganty@gmail.com>
2022-01-02 14:22:44 -0600

    change google callback back to local run for github push

--- routes/auth.ts
+++ routes/auth.ts
@@ -13,7 +13,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_HEROKU_CALLBACK /*process.env.GOOGLE_LOCAL_CALLBACK*/
+    callbackURL: /*process.env.GOOGLE_HEROKU_CALLBACK*/ process.env.GOOGLE_LOCAL_CALLBACK
   },
   function(accessToken: any, refreshToken: any, profile: any, done: any) {
     const user = ihubController.addUser({ 
